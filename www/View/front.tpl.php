3ce3edffd57ec7068d56b9b83284236b1c89fb03
marouaneTalbi <talbi.918marouane@gmail.com>
2022-05-23 21:28:04 +0200

    debeuger le fichier grnt.tpl.php

--- www/View/front.tpl.php
+++ www/View/front.tpl.php
@@ -10,6 +10,9 @@
 <body>
     <?php 
         include $this->view.".view.php";
+
+
+        
     ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="crossorigin="anonymous"></script>
     <script async src="../style/js/calendar.js"></script>
