f651e998c7b6b68d30803b2a1ab86d18103cf9c7
marouaneTalbi <talbi.918marouane@gmail.com>
2022-05-23 21:30:28 +0200

    debeuger

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -9,12 +9,6 @@ use App\Core\Validator;
 use App\Core\View;
 use App\Core\CheckInputs;
 use App\Model\User as UserModel;
-<<<<<<< HEAD
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-=======
->>>>>>> feature/rdv
 
 class User{
 
