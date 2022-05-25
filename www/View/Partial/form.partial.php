bbb19017d20855a6959c6c0a6effbc9381706d51
DregerTi <rui.soares282@gmail.com>
2022-05-25 10:40:05 +0200

    fix form builder

--- www/View/Partial/form.partial.php
+++ www/View/Partial/form.partial.php
@@ -34,49 +34,6 @@
                     <?php endforeach;?>
                 </select>
             </div>
-        <input name="<?=$name?>"
-        id="<?=$input["id"]?>"
-        type="<?=$input["type"]?>"
-        class="<?=$input["class"]?>"
-
-        <?php if(isset($input["value"])): ?>
-            value="<?=$input["value"]?>"
-        <?php endif ?>
-        <?php if(isset($input["placeholder"])): ?>
-            placeholder="<?=$input["placeholder"]?>"
-        <?php endif ?>
-        <?= (!empty($input["required"]))?'required="required"':'' ?>
-        >
-        <br>
-        <?php if(isset($config["select"])): ?>
-            <select 
-                id="<?=$config["select"]["title"]['id']?>"
-                type="<?=$config["select"]["title"]["type"]?>"
-                class="<?=$config["select"]["title"]["class"]?>"
-                name="<?=$config["select"]["title"]["name"]?>"
-            >
-            <?php foreach ($config["select"]["title"]["option"] as $option):  ?>
-                <option value="<?= $option ?>" > <?= $option ?></option>
-            <?php endforeach; ?>
-            </select>
-        <?php endif ?>
-        <br>
-        <?php if(isset($config["textarea"])): ?>
-            <textarea
-                type="<?=$config["textarea"]["description"]["type"] ?>"
-                placeholder="<?=$config["textarea"]["description"]["placeholder"] ?>"
-                id="<?=$config["textarea"]["description"]["type"] ?>"
-                name="<?=$config["textarea"]["description"]["name"] ?>"
-                class="<?=$config["textarea"]["description"]["class"] ?>"
-                min="<?=$config["textarea"]["description"]["min"] ?>"
-                max="<?=$config["textarea"]["description"]["max"] ?>"
-                error="<?=$config["textarea"]["description"]["error"] ?>"
-            >
-            <?=$config["textarea"]["description"]["value"] ?>
-            </textarea>
-        <?php endif ?>
-    </br>
-</form>
         <?php elseif ($input["type"] == "textarea"):?>
             <div class="input-container">
                 <label for="<?=$name?>"><?=$name?></label>
@@ -92,7 +49,6 @@
                 ><?= $input["value"] ?>
                 </textarea>
             </div>
-
         <?php else:?>
             <div class="input-container">
                 <label for="<?=$name?>"><?=$input["label"]?></label>
