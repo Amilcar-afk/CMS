b10cabe796f964901efffc6234334dbf38d2eb92
DregerTi <rui.soares282@gmail.com>
2022-06-06 21:50:46 +0200

    Update User.class.php

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -1,9 +1,13 @@
 <?php
 
+
 namespace App\Controller;
 
+
+use App\Core\BaseSQL;
 use App\Core\Validator;
 use App\Core\View;
+use App\Core\CheckInputs;
 use App\Model\User as UserModel;
 
 class User{
@@ -17,32 +21,27 @@ class User{
     public function login()
     {
         if( !empty($_POST)){
-            $result = Validator::checkEmail($_POST['email']);
-            if($result){
-
-                $this->user->setMail($_POST['email']);
-                $user = $this->user->find($this->user->getMail(), "mail");
-                if(!empty($user)){
-                    if(password_verify($_POST['password'], $user->getPwd())){
-                        session_start();
-                        $_SESSION['Auth']->mail = $user->getMail();
-                        $_SESSION['Auth']->lastname = $user->getLastname();
-                        $_SESSION['Auth']->firstname = $user->getFirstname();
-                        $_SESSION['Auth']->id = $user->getId();
-                        $_SESSION['Auth']->token = $user->getToken();
-                        $_SESSION['Auth']->creationDate = $user->getCreationDate();
-                        $_SESSION['Auth']->updateDate = $user->getUpdateDate();
-                        $_SESSION['Auth']->rank = $user->getRank();
-                        header('location:/dashboard');
-                    }else{
-                        echo'mot de passe incorrect';
-                    }
+            $this->user->setMail($_POST['email']);
+            $user = $this->user->find($this->user->getMail(), "mail");
+            if(!empty($user)){
+                if(password_verify($_POST['password'], $user->getPwd())){
+                    session_start();
+                    $_SESSION['Auth']->mail = $user->getMail();
+                    $_SESSION['Auth']->lastname = $user->getLastname();
+                    $_SESSION['Auth']->firstname = $user->getFirstname();
+                    $_SESSION['Auth']->id = $user->getId();
+                    $_SESSION['Auth']->token = $user->getToken();
+                    $_SESSION['Auth']->creationDate = $user->getCreationDate();
+                    $_SESSION['Auth']->updateDate = $user->getUpdateDate();
+                    $_SESSION['Auth']->rank = $user->getRank();
+                    header('location:/dashboard');
                 }else{
-                    echo'email ou mot passe incorrect';
+                    echo'mot de passe incorrect';
                 }
             }else{
-                echo'email incorrect';
+                echo'email ou mot passe incorrect';
             }
+
         }
         $view = new View("login", "back-sandbox");
         $view->assign("user",$this->user);
@@ -56,26 +55,21 @@ class User{
     }
 
 
-
     public function register()
     {
-        $view = new View("register", "back-sandbox");
-        $view->assign("user",$this->user);
         if( !empty($_POST)){
-            $this->user->setFirstname($_POST['firstname']);
-            $this->user->setLastname($_POST['lastname']);
-            $this->user->setPassword($_POST['password']);
-            $this->user->setMail($_POST['email']);
-
-            $unic_email = $this->user->find($_POST['email'],'mail');
-            $result = Validator::run($this->user->getFormRegister(), $_POST,$unic_email);
-            
+            $result = Validator::run($this->user->getFormRegister(), $_POST);
             if(empty($result)){
+                $this->user->setFirstname($_POST['firstname']);
+                $this->user->setLastname($_POST['lastname']);
+                $this->user->setPassword($_POST['password']);
+                $this->user->setEmail($_POST['email']);
                 $this->user->save();
-            }
-            else{
-                $view->assign("error_from",$result);
+            }else{
+                print_r($result);
             }
         }
+        $view = new View("register", "back-sandbox");
+        $view->assign("user",$this->user);
     }
 }
