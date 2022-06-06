84b5291be437012a1335bd0845e7668425ef54cb
DregerTi <rui.soares282@gmail.com>
2022-06-06 22:15:35 +0200

    Revert "Update User.class.php"
    This reverts commit b10cabe796f964901efffc6234334dbf38d2eb92.

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -1,13 +1,9 @@
 <?php
 
-
 namespace App\Controller;
 
-
-use App\Core\BaseSQL;
 use App\Core\Validator;
 use App\Core\View;
-use App\Core\CheckInputs;
 use App\Model\User as UserModel;
 
 class User{
@@ -21,27 +17,32 @@ class User{
     public function login()
     {
         if( !empty($_POST)){
-            $this->user->setMail($_POST['email']);
-            $user = $this->user->find($this->user->getMail(), "mail");
-            if(!empty($user)){
-                if(password_verify($_POST['password'], $user->getPwd())){
-                    session_start();
-                    $_SESSION['Auth']->mail = $user->getMail();
-                    $_SESSION['Auth']->lastname = $user->getLastname();
-                    $_SESSION['Auth']->firstname = $user->getFirstname();
-                    $_SESSION['Auth']->id = $user->getId();
-                    $_SESSION['Auth']->token = $user->getToken();
-                    $_SESSION['Auth']->creationDate = $user->getCreationDate();
-                    $_SESSION['Auth']->updateDate = $user->getUpdateDate();
-                    $_SESSION['Auth']->rank = $user->getRank();
-                    header('location:/dashboard');
+            $result = Validator::checkEmail($_POST['email']);
+            if($result){
+
+                $this->user->setMail($_POST['email']);
+                $user = $this->user->find($this->user->getMail(), "mail");
+                if(!empty($user)){
+                    if(password_verify($_POST['password'], $user->getPwd())){
+                        session_start();
+                        $_SESSION['Auth']->mail = $user->getMail();
+                        $_SESSION['Auth']->lastname = $user->getLastname();
+                        $_SESSION['Auth']->firstname = $user->getFirstname();
+                        $_SESSION['Auth']->id = $user->getId();
+                        $_SESSION['Auth']->token = $user->getToken();
+                        $_SESSION['Auth']->creationDate = $user->getCreationDate();
+                        $_SESSION['Auth']->updateDate = $user->getUpdateDate();
+                        $_SESSION['Auth']->rank = $user->getRank();
+                        header('location:/dashboard');
+                    }else{
+                        echo'mot de passe incorrect';
+                    }
                 }else{
-                    echo'mot de passe incorrect';
+                    echo'email ou mot passe incorrect';
                 }
             }else{
-                echo'email ou mot passe incorrect';
+                echo'email incorrect';
             }
-
         }
         $view = new View("login", "back-sandbox");
         $view->assign("user",$this->user);
@@ -55,21 +56,26 @@ class User{
     }
 
 
+
     public function register()
     {
-        if( !empty($_POST)){
-            $result = Validator::run($this->user->getFormRegister(), $_POST);
-            if(empty($result)){
-                $this->user->setFirstname($_POST['firstname']);
-                $this->user->setLastname($_POST['lastname']);
-                $this->user->setPassword($_POST['password']);
-                $this->user->setEmail($_POST['email']);
-                $this->user->save();
-            }else{
-                print_r($result);
-            }
-        }
         $view = new View("register", "back-sandbox");
         $view->assign("user",$this->user);
+        if( !empty($_POST)){
+            $this->user->setFirstname($_POST['firstname']);
+            $this->user->setLastname($_POST['lastname']);
+            $this->user->setPassword($_POST['password']);
+            $this->user->setMail($_POST['email']);
+
+            $unic_email = $this->user->find($_POST['email'],'mail');
+            $result = Validator::run($this->user->getFormRegister(), $_POST,$unic_email);
+            
+            if(empty($result)){
+                $this->user->save();
+            }
+            else{
+                $view->assign("error_from",$result);
+            }
+        }
     }
 }
