212cbabf8700027a11ee0553c60b8df4f780dcf2
DregerTi <rui.soares282@gmail.com>
2022-06-08 01:17:19 +0200

    START UPDATE Categorie

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -13,6 +13,7 @@
         <script type="text/javascript" src="../style/js/utilsMenu.js"></script>
         <script type="text/javascript" src="../style/js/wysiwyg.js"></script>
         <script type="text/javascript" src="../style/js/animations.js"></script>
+        <script type="text/javascript" src="../style/js/pages.js"></script>
     <head>
     <div class="background-container-back-office">
         <div></div>
