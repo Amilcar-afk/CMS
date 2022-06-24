d8e78a9dd9c85cdd257fab934f56d7d6aa77861d
Amilcar-afk <67441385+Amilcar-afk@users.noreply.github.com>
2022-06-24 21:26:26 +0200

    project model

---
+++ www/Model/Project.class.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace App\Model;
+
+use App\Core\BaseSQL;
+
+
+class Project extends BaseSQL
+{
+    protected $id = null;
+    protected $user_key = null;
+    protected $page_key = null;
+    protected $type;
+    protected $title;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle(): ? string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): ? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function save()
+    {
+        parent::save();
+    }
+
+    public function delete($id)
+    {
+        return parent::delete($id);
+
+    }
+}
