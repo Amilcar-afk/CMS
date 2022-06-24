58b42a7d2a69d875521dfad5880af27ea4bb26e5
Amilcar-afk <67441385+Amilcar-afk@users.noreply.github.com>
2022-06-24 16:35:32 +0200

    token session ok

--- www/Controller/Middleware.class.php
+++ www/Controller/Middleware.class.php
@@ -1,12 +1,26 @@
 <?php
 namespace App\Controller;
 
+use App\Core\Query;
+
 session_start();
 
 
 class Middleware{
 
     public static function auth(){
+        if(!empty($_SESSION['Auth']->token) && !empty($_SESSION['Auth']->id)){
+
+            $tokenVerif = Query::from('cmspf_Users')
+                ->where("token = '" . $_SESSION['Auth']->token . "'")
+                ->where("confirm = '1'")
+                ->execute("User");
+
+            if (!count($tokenVerif) > 0){
+                header('location:/login');
+            }
+        }
+
         if(!isset($_SESSION['Auth']->rank)){
             if($_SERVER['REQUEST_URI'] !== '/logout' && $_SERVER['REQUEST_URI'] !== '/login' ){
                 $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI'];
