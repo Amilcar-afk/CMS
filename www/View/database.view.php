ed0d0f0515131a79adcc612f0be8e98c93b3e7ac
DregerTi <rui.soares282@gmail.com>
2022-06-25 18:17:44 +0200

    fix config -> Configuration

--- www/View/database.view.php
+++ www/View/database.view.php
@@ -3,9 +3,9 @@
         <div class="menu-container">
             <h1 class="title title--main-color place-menu">SETTINGS</h1>
             <nav>
-                <a href="/settings/user-manager" class="cta-button cta-button--menu main-nav-choice selected" data-wc-target="user-manager-container"><span class="material-icons-round">supervisor_account</span>User manager</a>
+                <a href="/settings/user-manager" class="cta-button cta-button--menu main-nav-choice" data-wc-target="user-manager-container"><span class="material-icons-round">supervisor_account</span>User manager</a>
                 <a href="/settings/style" class="cta-button cta-button--menu main-nav-choice" data-wc-target="style-container"><span class="material-icons-round">brush</span>Style</a>
-                <a href="/settings/configuration" class="cta-button cta-button--menu main-nav-choice" data-wc-target="database-container"><span class="material-icons-round">cloud</span>Config</a>
+                <a href="/settings/configuration" class="cta-button cta-button--menu main-nav- selected" data-wc-target="database-container"><span class="material-icons-round">cloud</span>Configuration</a>
                 <a href="/settings/media-library" class="cta-button cta-button--menu main-nav-choice" data-wc-target="media-library-container"><span class="material-icons-round">video_library</span>Media library</a>
             </nav>
         </div>
