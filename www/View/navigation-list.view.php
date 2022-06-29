c661c9527b2eee62607d2e7b6c51ceb17da11115
DregerTi <rui.soares282@gmail.com>
2022-06-29 02:50:01 +0200

    desable add nav

--- www/View/navigation-list.view.php
+++ www/View/navigation-list.view.php
@@ -18,9 +18,9 @@
 
                 <!--Add navigation-->
                 <article>
-                    <button class="cta-button cta-button-a cta-button--submit cta-button--submit--add" data-a-target="container-new-navigation">
+                    <!--<button class="cta-button cta-button-a cta-button--submit cta-button--submit--add" data-a-target="container-new-navigation">
                         New navigation
-                    </button>
+                    </button>-->
                     <div id="new-navigations-elements" class="container-main-content container-main-content--list collapse row" data-group-collapse="navigation-manager-container">
                     </div>
                 </article>
@@ -36,7 +36,6 @@
                                     </td>
                                     <td>
                                         <a href="/build/navigation/<?= $navigation->getId() ?>" class="cta-button"><span class="material-icons-round">mode</span></a>
-                                        <button class="cta-button cta-button-a" data-a-target="container-setting-navigation-<?=$navigation->getId() ?>"><span class="material-icons-round">build</span></button>
                                     </td>
                                 </tr>
                             <?php endforeach;?>
@@ -51,31 +50,31 @@
         </section>
     </section>
 
-    <?php foreach ($navigations as $navigation):?>
+    <?php // foreach ($navigations as $navigation):?>
         <!-- update navigations form -->
-        <section id="container-setting-navigation-<?=$navigation->getId() ?>" class="container-main-content container-main-content--menu a-zoom-out-end">
-            <button id="cta-button-close-container-setting-navigation-<?=$navigation->getId() ?>" class="cta-button cta-button--icon cta-button-a" data-a-target="container-setting-navigation-<?=$navigation->getId() ?>"><span class="material-icons-round">close</span></button>
+        <!--<section id="container-setting-navigation-<?/*=//$navigation->getId() */?>" class="container-main-content container-main-content--menu a-zoom-out-end">
+            <button id="cta-button-close-container-setting-navigation-<?/*=//$navigation->getId() */?>" class="cta-button cta-button--icon cta-button-a" data-a-target="container-setting-navigation-<?/*=//$navigation->getId() */?>"><span class="material-icons-round">close</span></button>
             <div class="menu-container">
 
             </div>
             <section class="collapse-parent">
                 <div id="text-elements-container" class="collapse--open" data-group-collapse="add-elements-conatiner">
                     <header>
-                        <h1 class="title title--black"><?= ucfirst($navigation->getTitle()) ?></h1>
+                        <h1 class="title title--black"><?/*= //ucfirst($navigation->getTitle()) */?></h1>
                     </header>
 
                     <article>
-                        <?php  $this->includePartial("form", $navigation->getFormNewCategorie()) ?>
+                        <?php /*// $this->includePartial("form", $navigation->getFormNewCategorie()) */?>
                     </article>
 
                 </div>
 
             </section>
-        </section>
-    <?php endforeach;?>
+        </section>-->
+    <?php // endforeach;?>
 
     <!-- add navigations form -->
-    <section id="container-new-navigation" class="container-main-content container-main-content--menu a-zoom-out-end">
+    <!--<section id="container-new-navigation" class="container-main-content container-main-content--menu a-zoom-out-end">
         <button id="cta-button-close-container-new-navigation" class="cta-button cta-button--icon cta-button-a" data-a-target="container-new-navigation"><span class="material-icons-round">close</span></button>
         <div class="menu-container">
 
@@ -87,12 +86,12 @@
                 </header>
 
                 <article>
-                    <?php  $this->includePartial("form", $navigation->getFormNewCategorie()) ?>
+                    <?php /* //$this->includePartial("form", $navigation->getFormNewCategorie()) */?>
                 </article>
 
             </div>
 
         </section>
-    </section>
+    </section>-->
 
 </section>
