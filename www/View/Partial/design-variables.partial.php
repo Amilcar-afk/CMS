78451203c520ce99efbafa6067936c8257a12c49
DregerTi <rui.soares282@gmail.com>
2022-07-07 23:11:46 +0200

    add colors in js

--- www/View/Partial/design-variables.partial.php
+++ www/View/Partial/design-variables.partial.php
@@ -7,6 +7,11 @@
          '<?= $font->getValue() ?>',
     <?php endforeach;?>
     ];
+
+    let mainColor = <?= (isset($mainColor[0]))? $mainColor[0]->getValue() :'#396075' ?>;
+    let secondColor = <?= (isset($secondColor[0]) )? $secondColor[0]->getValue() :'#55A6D3' ?>;
+    let thirdColor = <?= (isset(thirdColor[0]) )? thirdColor[0]->getValue() :'#55A6D3' ?>;
+
 </script>
 <style>
     <?php foreach ($fonts as $font):?>
@@ -19,9 +24,9 @@
     <?php endforeach;?>
 
     :root {
-    --main-color: <?= (isset($mainColor[0]))? $mainColor[0]->getValue() :'#396075' ?>;
-    --second-color: <?= (isset($secondColor[0]) )? $secondColor[0]->getValue() :'#55A6D3' ?>;
-    --third-color: <?= (isset($thirdColor[0]) )? $thirdColor[0]->getValue() :'#9BBCFF' ?>;
+    --main-color: <?= (isset($mainColor[0]))? "'". $mainColor[0]->getValue() . "'" :'#396075' ?>;
+    --second-color: <?= (isset($secondColor[0]) )? "'". $secondColor[0]->getValue() . "'" :'#55A6D3' ?>;
+    --third-color: <?= (isset($thirdColor[0]) )? "'". $thirdColor[0]->getValue() . "'" :'#55A6D3' ?>;
 
     --main-color-rgb: 57,96,117;
     --second-color-rgb: 85,166,211;
