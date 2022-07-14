3df4bec62f5f3607ea2960683c0846e2e509d095
DregerTi <rui.soares282@gmail.com>
2022-07-14 19:15:44 +0200

    error page

--- www/View/setup/database.view.php
+++ www/View/setup/database.view.php
@@ -1,4 +1,19 @@
 <main>
+    <style>
+        :root {
+            --main-color: #626262;
+            --second-color: #afafaf;
+            --third-color: #979797;
+
+            --main-color-rgb: 57,96,117;
+            --second-color-rgb: 85,166,211;
+            --third-color-rgb: 155,188,255;
+
+            --background-color: #ffffff;
+            --text-color: black;
+
+        }
+    </style>
     <section class="container-main-content container-main-content--setup" >
         <header>
             <figure id="back-office-logo"><img src="<?= (isset($logo[0])) ? $logo[0]->getPath() :'/style/images/logo_myfolio.png'  ?>" alt="logo"></figure>
