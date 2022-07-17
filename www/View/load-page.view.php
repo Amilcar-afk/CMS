a24fb6cd4331a94b9ed738d2d95fa79d35854029
DregerTi <rui.soares282@gmail.com>
2022-07-18 01:48:28 +0200

    fix reponsivity & addcode isset & images

--- www/View/load-page.view.php
+++ www/View/load-page.view.php
@@ -1,12 +1,6 @@
-<?php if(isset($background[0]) && $background[0]->getValue() == 'yes' ): ?>
-    <div class="background-container-back-office background-container-back-office--go">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-<?php endif; ?>
 <?= $page->header()?>
-<div class="row <?= (isset($bessels[0]) && $bessels[0]->getValue() != null )? $bessels[0]->getValue() : 'body-medium' ?>"><?=$page->getContent()?></div>
+<div class="row <?= (isset($bessels[0]) && $bessels[0]->getValue() != null )? $bessels[0]->getValue() : 'body-medium' ?>">
+    <?=$page->getContent()?>
+</div>
 <?= $page->footer()?>
 
