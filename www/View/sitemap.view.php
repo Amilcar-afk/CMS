d7d456ec800e1470e459eba862e7aef8d6d72f5b
Debian <debian@vps-9b22e5bc.vps.ovh.net>
2022-07-21 23:46:47 +0000

    ok

--- www/View/sitemap.view.php
+++ www/View/sitemap.view.php
@@ -1,2 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>http://localhost/</loc><lastmod/></url><url><loc>http://localhost/slug1</loc><lastmod/></url><url><loc>http://localhost/slug2</loc><lastmod>2024-06-22 10:56:27</lastmod></url></urlset>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>http://ruimiguelsoares.fr/ok</loc><lastmod>2014-07-22</lastmod></url><url><loc>http://ruimiguelsoares.fr/</loc><lastmod/></url></urlset>
